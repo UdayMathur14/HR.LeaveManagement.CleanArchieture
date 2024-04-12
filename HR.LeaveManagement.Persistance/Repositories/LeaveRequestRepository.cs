e83e2bfdfd0855dd4ce492ec91871d731f3191fc
UdayDiverse <uday.m@diverseinfotech.com>
2024-04-13 01:36:33 +0530

    remove extra usings

--- HR.LeaveManagement.Persistance/Repositories/LeaveRequestRepository.cs
+++ HR.LeaveManagement.Persistance/Repositories/LeaveRequestRepository.cs
@@ -2,11 +2,6 @@
 using HR.LeaveManagement.Domain;
 using HR.LeaveManagement.Persistance.DataBaseContext;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Persistance.Repositories
 {
